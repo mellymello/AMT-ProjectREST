12d14c150bc83f4a2a9d90094038502200d485e6
calixte.melly <calixte.melly@heig-vd.ch>
2014-12-11 15:19:52 +0100

    script OK

--- testAPI.js
+++ testAPI.js
@@ -3,35 +3,38 @@ var http = require('http');
 var fs = require('fs');
 
 
-var port = 80; 
-var hostname = 'http://localhost'; 
-var pathGETObs = '/api/observations'; 
-var pathPOSTObs = '/api/observations'; 
+var port = 8080; 
+var hostname = '10.192.86.146';  
+var pathPOSTObs = '/amtProject/api/observations'; 
 
+var postObs = function(id){
+	var post_data = '{"time":"'+new Date().toJSON()+'", "value":'+Math.floor((Math.random() * 100) + 1)+',"sensor":'+null+'}'; 
+	  // An object of options to indicate where to post to
+	  var post_options = {
+		  host: hostname,
+		  port: port,
+		  path: pathPOSTObs,
+		  method: 'POST',
+		  headers: {
+			  'Content-Type': 'application/json',
+			  'Content-Length': post_data.length
+		  }
+	  };
 
-var post_data =[{"_id":ids[i]._id+}, {"date":new Date().getTime()}, {"value":Math.floor((Math.random() * 100) + 1)}]; 
-  // An object of options to indicate where to post to
-  var post_options = {
-      host: hostname,
-      port: port,
-      path: pathPOSTObs,
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-          'Content-Length': post_data.length
-      }
-  };
+	  // Set up the request
+	  var post_req = http.request(post_options, function(res) {
+		  res.setEncoding('utf8');
+		  res.on('data', function (chunk) {
+			  console.log('Response: ' + chunk);
+		  });
+	  });
 
-  // Set up the request
-  var post_req = http.request(post_options, function(res) {
-      res.setEncoding('utf8');
-      res.on('data', function (chunk) {
-          console.log('Response: ' + chunk);
-      });
-  });
+	  // post the data
+	  post_req.write(post_data);
+	  post_req.end();
 
-  // post the data
-  post_req.write(post_data);
-  post_req.end();
-
+}
+for (var i = 0; i < 1000; i++)
+{
+	postObs(i);
 }
